552f1b025e77d6e9144e1395285ee8879b934e2d
Jonathan Svärdén <jonathan.xx.svarden@softhouse.se>
2015-03-25 13:07:42 +0100

    Add app scaffold

---
+++ src/js/components/app.js
@@ -0,0 +1,14 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+
+var APP = React.createClass({
+
+        render: function() {
+            return(
+                <h1>React boilerplate</h1>
+            );
+        }
+    });
+
+module.exports = APP;
